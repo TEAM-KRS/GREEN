ce41772f0d4adaf781f2615dfc1d05c678c888a7
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2022-09-26 12:21:35 +0500

    Create Krs.py

---
+++ Krs.py
@@ -0,0 +1,8 @@
+import os
+os.system("clear")
+os.system('xdg-open https://facebook.com/groups/207678473842318/')
+print("")
+print("\t\033[1;31mPLEASE W8 THIS TOOL WAS UPDATING")
+print (" ")
+print(" \tTHANKS FOR USING MY TOOL ")
+print(" ")
